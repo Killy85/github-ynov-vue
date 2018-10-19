21d1de2a646770c93a61be283abc83004adab6f9
Killy Gourou <goureau.n.a@hotmail.fr>
2018-10-19 09:56:47 +0200

    Adding boostrap and Jquery. Now can select people/project

--- index.js
+++ index.js
@@ -21,7 +21,6 @@
         return xmlhttp.responseText;
       }
       else {
-        // TODO Throw exception
         return null;
       }
     }
@@ -31,26 +30,44 @@
   urls_count = 0
   urls_fetched =0
 
-  function getRepos(app){
+  function getRepos(){
       urls = []
-      search.projects.forEach(project => {
+      repos = []
+      if(app.selected_project && app.selected_user){
+        urls.push(settings.baseURL + "repos/"+app.selected_user +"/"+ app.selected_project)
+      }else if(app.selected_project) {
         search.users.forEach(user => {   
-          urls.push(settings.baseURL + "repos/"+user +"/"+ project)
+          urls.push(settings.baseURL + "repos/"+user +"/"+ app.selected_project)
         });
-      });
-      urls_count = urls.length
+      }else if( app.selected_user){
+        search.projects.forEach(project => {
+          urls.push(settings.baseURL + "repos/"+app.selected_user +"/"+ project)
+        });
+      }else {
+        search.projects.forEach(project => {
+          search.users.forEach(user => {   
+            urls.push(settings.baseURL + "repos/"+user +"/"+ project)
+          });
+        });
+      }
+      urls_count = urls.length - 1 
       urls.forEach(url => {
         get_repos(url, (status, repository) => {
           get_commits(url,(status,commits_to_rep)=>{
-          urls_fetched++;
-          if(!status){
-            val = repository
-            val["commits"] = commits_to_rep
-            repos.push(val)
-          }    
-          if(urls_count === urls_fetched){
-            app.repo = repos
-          } 
+            get_readme(url, (status,readme) => {
+              urls_fetched++;
+              if(!status){
+                val = repository
+                val["commits"] = commits_to_rep
+                val["readme"] = readme
+                console.log(val)
+                repos.push(val)
+              }
+              if(urls_count <= urls_fetched){
+                app.repo = repos
+                urls_fetched = 0
+              } 
+            })
           }) 
       })
       })
@@ -87,12 +104,43 @@
       }
     };
     xhr.send();
+  };
+
+  var get_readme = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url + "/readme", true);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader("Authorization", "Basic " + btoa(settings.credentials.login+":"+settings.credentials.password))
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
 };
 
-  var app = new Vue({
+
+var app = new Vue({
       el: '#feed',
       data: {
-        repo : repos
+        repo : repos,
+        users : search.users,
+        projects : search.projects,
+        selected_user : null,
+        selected_project : null,
+        date_from : null,
+        date_to : null
+      },
+      watch: {
+        selected_project : (val) => { getRepos(this)},
+        selected_user : (val) => { getRepos(this)},
+        date_from : (val) => { getRepos(this)},
+        date_to : (val) => { getRepos(this)}
       }
-    })
-    getRepos(app)
+})
+
+
+getRepos(app)
